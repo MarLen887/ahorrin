87f8d5ce9f8a2a66aa47f08cfcc2fe8391829b42
Miriam47029 <ti47029@uvp.edu.mx>
2025-06-03 21:17:59 -0600

    vistaBancos

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -8,33 +8,70 @@
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/all.min.css')}} ">
     <link rel="stylesheet" href="{{ asset('css/personalizacion.css')}} ">
+
+    {{-- Opcional: Si estás usando SweetAlert2, también deberías considerar su CSS aquí --}}
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css"> --}}
+
+    {{-- Puedes añadir tus estilos CSS personalizados aquí o en personalizacion.css --}}
+    <style>
+        .btn-color-custom-darkblue {
+            background-color: #0B2C79; /* Un azul oscuro */
+            color: #fff;
+        }
+        .btn-color-custom-darkblue:hover {
+            background-color: #082260; /* Un azul más oscuro para hover */
+            color: #fff;
+        }
+        .btn-color-custom-cerulean {
+            background-color: #2A61AE; /* Un azul cerúleo */
+            color: #fff;
+        }
+        .btn-color-custom-cerulean:hover {
+            background-color: #204b8a; /* Un azul más oscuro para hover */
+            color: #fff;
+        }
+        .seccion-principal {
+            padding: 20px;
+        }
+        .tabla-estilizada thead {
+            background-color: #e9ecef;
+            color: #495057;
+        }
+        .tabla-estilizada th, .tabla-estilizada td {
+            vertical-align: middle;
+        }
+    </style>
 </head>
 <body>
-    <!--inicia navbar-->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Cartera Inteligente</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/bancos">Bancos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="/operaciones">Operación</a>
-        </li>
-      </ul>
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Cartera Inteligente</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link @if(Request::is('bancos') || Request::is('bancos/*')) active @endif" aria-current="page" href="{{ route('bancos.index') }}">Bancos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link @if(Request::is('operaciones') || Request::is('operaciones/*')) active @endif" href="{{ url('/operaciones') }}">Operación</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        @yield('contenido')
     </div>
-  </div>
-</nav>
-<!--fin-->
-<!--inicia div principal-->
-<div class="container-fluid">
-    @yield('contenido')
-</div>
-<!--fin-->
+    {{-- ¡AQUÍ ESTÁ LA SOLUCIÓN! --}}
+    {{-- Carga de Bootstrap JS --}}
+    <script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    {{-- Carga de SweetAlert2 JS si lo usas --}}
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    {{-- ¡MUY IMPORTANTE! Aquí se inyectarán los scripts de las vistas individuales como bancos.blade.php --}}
+    @yield('scripts')
+
 </body>
-<script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </html>
